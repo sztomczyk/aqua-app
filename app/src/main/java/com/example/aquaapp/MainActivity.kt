f22c573df6d38074456f0be6eef00d738c21b9e1
Szymon Tomczyk <szymon.tomczyk26@gmail.com>
2022-11-27 14:59:08 +0100

    add picking from available fish types and simlution works when fish type changes

--- app/src/main/java/com/example/aquaapp/MainActivity.kt
+++ app/src/main/java/com/example/aquaapp/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.aquaapp
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.IdRes
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private var connected = false
+    private var tempConditions = mapOf<String, Int>("gupik" to 20, "pawie_oczko" to 25, "black_molly" to 30)
+    private var lightConditions = mapOf<String, Int>("gupik" to 600, "pawie_oczko" to 660, "black_molly" to 700)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +28,50 @@ class MainActivity : AppCompatActivity() {
             connectionStatusValueLabel.text = if (connected) "Niepołączony" else "Połączony"
             connectionStatusValueLabel.setTextColor(if (connected) Color.RED else Color.GREEN)
             connected = !connected
-            simulation()
+
+            if (findViewById<View>(R.id.gupik_bar).visibility == View.VISIBLE) {
+                simulation("gupik")
+            } else if (findViewById<View>(R.id.pawie_oczko_bar).visibility == View.VISIBLE) {
+                simulation("pawie_oczko")
+            } else if (findViewById<View>(R.id.black_molly_bar).visibility == View.VISIBLE) {
+                simulation("black_molly")
+            }
+        }
+
+        val gupikLayout: LinearLayout = findViewById(R.id.gupik)
+        gupikLayout.setOnClickListener {
+            toggleBars(R.id.gupik_bar)
+            simulation("gupik")
+        }
+
+        val pawieOczkoLayout: LinearLayout = findViewById(R.id.pawie_oczko)
+        pawieOczkoLayout.setOnClickListener {
+            toggleBars(R.id.pawie_oczko_bar)
+            simulation("pawie_oczko")
+        }
+
+        val blackMollyLayout: LinearLayout = findViewById(R.id.black_molly)
+        blackMollyLayout.setOnClickListener {
+            toggleBars(R.id.black_molly_bar)
+            simulation("black_molly")
+        }
+
+        val dopasujLayout: LinearLayout = findViewById(R.id.dopasuj)
+        dopasujLayout.setOnClickListener {
+            toggleBars(R.id.dopasuj_bar)
         }
     }
 
-    fun simulation() {
+    fun toggleBars(id: Int) {
+        findViewById<View>(R.id.gupik_bar).visibility = View.INVISIBLE
+        findViewById<View>(R.id.pawie_oczko_bar).visibility = View.INVISIBLE
+        findViewById<View>(R.id.black_molly_bar).visibility = View.INVISIBLE
+        findViewById<View>(R.id.dopasuj_bar).visibility = View.INVISIBLE
+
+        findViewById<View>(id).visibility = View.VISIBLE
+    }
+
+    fun simulation(fishType: String) {
         val tempValueLabel: TextView = findViewById(R.id.tempValueLabel)
         val lightValueLabel: TextView = findViewById(R.id.lightValueLabel)
         val lastTempLabel: TextView = findViewById(R.id.lastTempLabel)
@@ -36,10 +80,21 @@ class MainActivity : AppCompatActivity() {
         thread {
             while (connected) {
                 runOnUiThread {
+                    val tempCondition = tempConditions[fishType]
+                    val lightCondition = lightConditions[fishType]
+
                     lastTempLabel.text = "Ostatni pomiar: " + tempValueLabel.text
                     lastLightLabel.text = "Ostatni pomiar: " + lightValueLabel.text
-                    tempValueLabel.text = Random.nextInt(27, 30).toString() + " °C"
-                    lightValueLabel.text = Random.nextInt(680, 720).toString() + " lux"
+                    tempValueLabel.text = tempCondition?.let {
+                        Random.nextInt(it - 2,
+                            it
+                        ).toString()
+                    } + " °C"
+                    lightValueLabel.text = lightCondition?.let {
+                        Random.nextInt(it - 10,
+                            it
+                        ).toString()
+                    } + " lux"
                 }
                 Thread.sleep(3000)
             }
